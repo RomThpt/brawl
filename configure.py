850b1c9eaa88aed03a8c94de272d4880192d93f4
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:40:07 +0200

    Match ft_fox trivial functions

--- configure.py
+++ configure.py
@@ -1087,6 +1087,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_fox/fn_97_B1A8.c"),
             Object(Matching, "mo_stub/ft_fox/fn_97_B1D0.c"),
             Object(Matching, "mo_stub/ft_fox/fn_97_B248.c"),
+            Object(Matching, "mo_stub/ft_fox/fn_97_B254.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_fox/fn_97_B254.c
@@ -0,0 +1,16 @@
+int fn_97_B254(void) {
+    return 0;
+}
+
+void fn_97_B25C(void) {
+}
+
+int fn_97_B260(void) {
+    return 0;
+}
+
+void fn_97_B268(void) {
+}
+
+void fn_97_B26C(void) {
+}
