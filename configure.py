768a8145ba55872b2bec639d460658060c950ee5
RomThpt <romain@xrpl-commons.org>
2026-07-17 20:20:21 +0200

    Match ft_iceclimber trivial functions

--- configure.py
+++ configure.py
@@ -1357,6 +1357,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_iceclimber/fn_105_18334.c"),
             Object(Matching, "mo_stub/ft_iceclimber/fn_105_183E0.c"),
             Object(Matching, "mo_stub/ft_iceclimber/fn_105_183EC.c"),
+            Object(Matching, "mo_stub/ft_iceclimber/fn_105_1841C.c"),
         ],
     },
     {
