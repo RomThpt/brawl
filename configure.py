e1487b9abe3ec646d860be54270fa952099df0f1
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:51:38 +0200

    Match ft_koopa trivial functions

--- configure.py
+++ configure.py
@@ -1219,7 +1219,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_koopa/fn_102_8EA4.c"),
+        ],
     },
     {
         "lib": "ft_link",
---
+++ src/mo_stub/ft_koopa/fn_102_8EA4.c
@@ -0,0 +1,14 @@
+void fn_102_8EA4(void) {
+}
+
+void fn_102_8EA8(void) {
+}
+
+void fn_102_8EAC(void) {
+}
+
+void fn_102_8EB0(void) {
+}
+
+void fn_102_8EB4(void) {
+}
