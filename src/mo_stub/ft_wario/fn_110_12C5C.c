3d70656aebb7f88a4eb4c6dcb8a6449f067b5904
RomThpt <romain@xrpl-commons.org>
2026-07-17 21:29:25 +0200

    Match ft_wario trivial functions

---
+++ src/mo_stub/ft_wario/fn_110_12C5C.c
@@ -0,0 +1,19 @@
+int fn_110_12C5C(void* p) {
+    return *(int*)((char*)p + 16);
+}
+
+int fn_110_12C64(void* p) {
+    return *(unsigned short*)((char*)p + 20);
+}
+
+int fn_110_12C6C(void* p) {
+    return *(int*)((char*)p + 8);
+}
+
+int fn_110_12C74(void* p) {
+    return *(unsigned short*)((char*)p + 22);
+}
+
+int fn_110_12C7C(void* p) {
+    return *(int*)((char*)p + 12);
+}
