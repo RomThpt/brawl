62be64ae2b9dba969210f360622441cab148a2ad
RomThpt <romain@xrpl-commons.org>
2026-07-17 20:11:56 +0200

    Match ft_fox trivial functions

---
+++ src/mo_stub/ft_fox/fn_97_10C8C.c
@@ -0,0 +1,19 @@
+int fn_97_10C8C(void* p) {
+    return *(int*)((char*)p + 16);
+}
+
+int fn_97_10C94(void* p) {
+    return *(int*)((char*)p + 16);
+}
+
+int fn_97_10C9C(void* p) {
+    return *(unsigned short*)((char*)p + 20);
+}
+
+int fn_97_10CA4(void* p) {
+    return *(int*)((char*)p + 8);
+}
+
+int fn_97_10CAC(void* p) {
+    return *(unsigned short*)((char*)p + 22);
+}
