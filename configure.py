b214890abf784b586bffea037c06b76a84edf5a0
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:36:53 +0200

    Match ft_falco trivial functions

--- configure.py
+++ configure.py
@@ -1050,7 +1050,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_falco/fn_108_AEC0.c"),
+        ],
     },
     {
         "lib": "ft_fox",
---
+++ src/mo_stub/ft_falco/fn_108_AEC0.c
@@ -0,0 +1,14 @@
+void fn_108_AEC0(void) {
+}
+
+void fn_108_AEC4(void) {
+}
+
+void fn_108_AEC8(void) {
+}
+
+void fn_108_AECC(void) {
+}
+
+void fn_108_AED0(void) {
+}
