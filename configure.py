f2d53a424fa2b2e5e4f356919c1e973b893419ba
RomThpt <romain@xrpl-commons.org>
2026-07-18 01:45:29 +0200

    Match ft_donkey trivial functions

--- configure.py
+++ configure.py
@@ -1222,6 +1222,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_donkey/fn_92_9724.c"),
             Object(Matching, "mo_stub/ft_donkey/fn_92_C11C.c"),
             Object(Matching, "mo_stub/ft_donkey/fn_92_C210.c"),
+            Object(Matching, "mo_stub/ft_donkey/fn_92_C2F8.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_donkey/fn_92_C2F8.c
@@ -0,0 +1,3 @@
+int fn_92_C2F8(void) {
+    return 0;
+}
