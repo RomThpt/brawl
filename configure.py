86ef5194192adfbc49b8e831181ad9b8377411b8
RomThpt <romain@xrpl-commons.org>
2026-07-17 20:22:44 +0200

    Match ft_ike trivial functions

--- configure.py
+++ configure.py
@@ -1400,6 +1400,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_ike/fn_119_134C8.c"),
             Object(Matching, "mo_stub/ft_ike/fn_119_134D4.c"),
             Object(Matching, "mo_stub/ft_ike/fn_119_13504.c"),
+            Object(Matching, "mo_stub/ft_ike/fn_119_1351C.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_ike/fn_119_1351C.c
@@ -0,0 +1,14 @@
+void fn_119_1351C(void) {
+}
+
+void fn_119_13520(void) {
+}
+
+void fn_119_13524(void) {
+}
+
+void fn_119_13528(void) {
+}
+
+void fn_119_1352C(void) {
+}
