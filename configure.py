25f08abfdcba6f8f9a65e04142742b3e2bdb01cf
RomThpt <romain@xrpl-commons.org>
2026-07-18 00:34:07 +0200

    Match ft_kirby trivial functions

--- configure.py
+++ configure.py
@@ -1559,6 +1559,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_kirby/fn_96_23CBC.c"),
             Object(Matching, "mo_stub/ft_kirby/fn_96_23CDC.c"),
             Object(Matching, "mo_stub/ft_kirby/fn_96_23D24.c"),
+            Object(Matching, "mo_stub/ft_kirby/fn_96_23D34.c"),
         ],
     },
     {
