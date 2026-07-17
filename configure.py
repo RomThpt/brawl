097d8fafc9f19bbb530a0b63e2274b41283edf2d
RomThpt <romain@xrpl-commons.org>
2026-07-17 15:54:14 +0200

    Match st_dxyorster trivial functions

--- configure.py
+++ configure.py
@@ -2188,6 +2188,8 @@ config.libs = [
             Object(Matching, "mo_stub/st_dxrcruise/fn_80_2A20.c"),
             Object(Matching, "mo_stub/st_dxyorster/fn_76_16C.c"),
             Object(Matching, "mo_stub/st_dxyorster/fn_76_4F4.c"),
+            Object(Matching, "mo_stub/st_dxyorster/fn_76_6E0.c"),
+            Object(Matching, "mo_stub/st_dxyorster/fn_76_854.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_dxyorster/fn_76_854.c
@@ -0,0 +1,11 @@
+void fn_76_854(void* p, int q) {
+    *(unsigned char*)((char*)p + 352) = q;
+}
+
+void fn_76_85C(void* p, int q) {
+    *(int*)((char*)p + 344) = q;
+}
+
+void fn_76_864(void* p, int q) {
+    *(int*)((char*)p + 360) = q;
+}
