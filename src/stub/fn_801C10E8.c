3c079d24332a1d8c4a21d644c7266d3dccc24b9f
RomThpt <romain@xrpl-commons.org>
2026-07-17 14:35:59 +0200

    Match trivial stub functions

---
+++ src/stub/fn_801C10E8.c
@@ -0,0 +1,11 @@
+int fn_801C10E8(void* p) {
+    return *(unsigned char*)((char*)p + 142);
+}
+
+int fn_801C10F0(void* p) {
+    return *(unsigned char*)((char*)p + 141);
+}
+
+int fn_801C10F8(void* p) {
+    return *(unsigned char*)((char*)p + 140);
+}
