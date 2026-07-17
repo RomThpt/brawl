9ce99d9d0d413481abefdb4449bfb16b2c9179e2
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:55:07 +0200

    Match ft_link trivial functions

--- configure.py
+++ configure.py
@@ -1254,6 +1254,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_link/fn_93_AFBC.c"),
             Object(Matching, "mo_stub/ft_link/fn_93_AFC0.c"),
             Object(Matching, "mo_stub/ft_link/fn_93_AFE8.c"),
+            Object(Matching, "mo_stub/ft_link/fn_93_B008.c"),
         ],
     },
     {
