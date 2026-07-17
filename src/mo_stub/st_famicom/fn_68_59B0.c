9b8cf75f5fe7b4ef84da2da0d89090ce2ac18b9f
RomThpt <romain@xrpl-commons.org>
2026-07-17 16:51:36 +0200

    Match st_famicom trivial functions

---
+++ src/mo_stub/st_famicom/fn_68_59B0.c
@@ -0,0 +1,17 @@
+void fn_68_59B0(void) {
+}
+
+int fn_68_59B4(void) {
+    return 0;
+}
+
+int fn_68_59BC(void) {
+    return 0;
+}
+
+void fn_68_59C4(void) {
+}
+
+int fn_68_59C8(void) {
+    return 0;
+}
