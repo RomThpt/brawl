84057e31800f5342cfff017e7e3b44aa8954196e
RomThpt <romain@xrpl-commons.org>
2026-07-17 19:06:09 +0200

    Match ft_ness trivial functions

--- configure.py
+++ configure.py
@@ -1474,7 +1474,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_ness/fn_101_D0C4.c"),
+        ],
     },
     {
         "lib": "ft_peach",
---
+++ src/mo_stub/ft_ness/fn_101_D0C4.c
@@ -0,0 +1,14 @@
+void fn_101_D0C4(void) {
+}
+
+void fn_101_D0C8(void) {
+}
+
+void fn_101_D0CC(void) {
+}
+
+void fn_101_D0D0(void) {
+}
+
+void fn_101_D0D4(void) {
+}
