618104f56a1c411612a4189a34c936ad2e3afac3
RomThpt <romain@xrpl-commons.org>
2026-07-17 14:45:24 +0200

    Match trivial stub functions

--- configure.py
+++ configure.py
@@ -280,6 +280,7 @@ config.libs = [
             Object(NonMatching, "Runtime.PPCEABI.H/__init_cpp_exceptions.cpp"),
             Object(Matching, "TRK_MINNOW_DOLPHIN/dispatch.c"),
             Object(Matching, "TRK_MINNOW_DOLPHIN/udp_cc.c"),
+            Object(Matching, "stub/fn_80298784.c"),
             Object(Matching, "stub/fn_8029875C.c"),
             Object(Matching, "stub/fn_80298744.c"),
             Object(Matching, "stub/fn_80298734.c"),
