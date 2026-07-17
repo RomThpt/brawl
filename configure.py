51e5bf1aea092844933f3fe938b0cd702fda3842
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:34:53 +0200

    Match ft_donkey trivial functions

--- configure.py
+++ configure.py
@@ -1025,7 +1025,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_donkey/fn_92_9728.c"),
+        ],
     },
     {
         "lib": "ft_falco",
---
+++ src/mo_stub/ft_donkey/fn_92_9728.c
@@ -0,0 +1,14 @@
+void fn_92_9728(void) {
+}
+
+void fn_92_972C(void) {
+}
+
+void fn_92_9730(void) {
+}
+
+void fn_92_9734(void) {
+}
+
+void fn_92_9738(void) {
+}
