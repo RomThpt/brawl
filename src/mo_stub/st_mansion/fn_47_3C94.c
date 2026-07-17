c1b57337bec8b840d4fd36e160c22cbf97cc6815
RomThpt <romain@xrpl-commons.org>
2026-07-17 17:03:19 +0200

    Match st_mansion trivial functions

---
+++ src/mo_stub/st_mansion/fn_47_3C94.c
@@ -0,0 +1,19 @@
+int fn_47_3C94(void) {
+    return 0;
+}
+
+int fn_47_3C9C(void) {
+    return 0;
+}
+
+int fn_47_3CA4(void) {
+    return 0;
+}
+
+int fn_47_3CAC(void) {
+    return 1;
+}
+
+int fn_47_3CB4(void) {
+    return 0;
+}
