442df67cd639454846798a35f31dcc967e840629
RomThpt <romain@xrpl-commons.org>
2026-07-17 19:32:50 +0200

    Match ft_yoshi trivial functions

--- configure.py
+++ configure.py
@@ -1846,7 +1846,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_yoshi/fn_95_B558.c"),
+        ],
     },
     {
         "lib": "ft_zako",
---
+++ src/mo_stub/ft_yoshi/fn_95_B558.c
@@ -0,0 +1,14 @@
+void fn_95_B558(void) {
+}
+
+void fn_95_B55C(void) {
+}
+
+void fn_95_B560(void) {
+}
+
+void fn_95_B564(void) {
+}
+
+void fn_95_B568(void) {
+}
