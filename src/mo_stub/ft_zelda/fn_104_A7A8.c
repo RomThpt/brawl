e9dd42a3147966847648824c44ba26238ac19e71
RomThpt <romain@xrpl-commons.org>
2026-07-17 19:37:45 +0200

    Match ft_zelda trivial functions

---
+++ src/mo_stub/ft_zelda/fn_104_A7A8.c
@@ -0,0 +1,16 @@
+void fn_104_A7A8(void) {
+}
+
+void fn_104_A7AC(void) {
+}
+
+int fn_104_A7B0(void) {
+    return 32766;
+}
+
+void fn_104_A7B8(void) {
+}
+
+int fn_104_A7BC(void) {
+    return 0;
+}
