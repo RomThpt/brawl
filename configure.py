c5c215514dc9ceb7cbcf21150d0becc3a9f1c419
RomThpt <romain@xrpl-commons.org>
2026-07-17 16:45:23 +0200

    Match st_donkey trivial functions

--- configure.py
+++ configure.py
@@ -2031,6 +2031,7 @@ config.libs = [
             Object(Matching, "mo_stub/st_donkey/fn_50_5A88.c"),
             Object(Matching, "mo_stub/st_donkey/fn_50_5D38.c"),
             Object(Matching, "mo_stub/st_donkey/fn_50_6248.c"),
+            Object(Matching, "mo_stub/st_donkey/fn_50_6250.c"),
         ],
     },
     {
