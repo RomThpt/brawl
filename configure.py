d3db7ecb817086cfbaaf6ef78702cdbbf24d1f75
RomThpt <romain@xrpl-commons.org>
2026-07-18 00:51:47 +0200

    Match ft_snake trivial functions

--- configure.py
+++ configure.py
@@ -2641,6 +2641,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_snake/fn_122_18E34.c"),
             Object(Matching, "mo_stub/ft_snake/fn_122_18E44.c"),
             Object(Matching, "mo_stub/ft_snake/fn_122_18E58.c"),
+            Object(Matching, "mo_stub/ft_snake/fn_122_18E70.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_snake/fn_122_18E70.c
@@ -0,0 +1,18 @@
+int fn_122_18E70(void* p) {
+    return *(int*)((char*)p + 12);
+}
+
+void fn_122_18E78(void) {
+}
+
+int fn_122_18E7C(void* p) {
+    return *(int*)((char*)p + 8);
+}
+
+void fn_122_18E84(void* p, int q) {
+    *(int*)((char*)p + 8) = q;
+}
+
+int fn_122_18E8C(void* p) {
+    return *(int*)((char*)p + 8);
+}
