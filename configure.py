281e4fd085d2e30d62690e3f3a76907a5da12081
RomThpt <romain@xrpl-commons.org>
2026-07-17 16:32:48 +0200

    Match st_madein trivial functions

--- configure.py
+++ configure.py
@@ -2723,6 +2723,8 @@ config.libs = [
             Object(Matching, "mo_stub/st_kart/fn_49_5B78.c"),
             Object(Matching, "mo_stub/st_kart/fn_49_5B90.c"),
             Object(Matching, "mo_stub/st_kart/fn_49_5BA0.c"),
+            Object(Matching, "mo_stub/st_madein/fn_65_69C0.c"),
+            Object(Matching, "mo_stub/st_madein/fn_65_69E0.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_madein/fn_65_69E0.c
@@ -0,0 +1,7 @@
+int fn_65_69E0(void) {
+    return 22;
+}
+
+int fn_65_69E8(void) {
+    return 1;
+}
