d4471e755c893f5933e848a5d23b89e798152d9d
RomThpt <romain@xrpl-commons.org>
2026-07-17 14:41:41 +0200

    Match trivial stub functions

---
+++ src/stub/fn_8027C10C.c
@@ -0,0 +1,3 @@
+int fn_8027C10C(void* p) {
+    return *(int*)((char*)p + 68);
+}
---
+++ src/stub/fn_8027C83C.c
@@ -0,0 +1,11 @@
+int fn_8027C83C(void* p) {
+    return *(int*)((char*)p + 0);
+}
+
+int fn_8027C844(void* p) {
+    return *(int*)((char*)p + 4);
+}
+
+int fn_8027C84C(void* p) {
+    return *(int*)((char*)p + 12);
+}
