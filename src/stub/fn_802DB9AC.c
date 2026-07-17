20ecb68c16de7e3eb1d853e1badce032f1375773
RomThpt <romain@xrpl-commons.org>
2026-07-17 14:48:48 +0200

    Match trivial stub functions

---
+++ src/stub/fn_802DB9AC.c
@@ -0,0 +1,7 @@
+int fn_802DB9AC(void* p) {
+    return *(int*)((char*)p + 0);
+}
+
+int fn_802DB9B4(void* p) {
+    return *(int*)((char*)p + 0);
+}
---
+++ src/stub/fn_802DBB04.c
@@ -0,0 +1,3 @@
+void fn_802DBB04(void* p, int q) {
+    *(int*)((char*)p + 0) = q;
+}
