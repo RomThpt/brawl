9489eafcf34ca4ccbdff400a82bf556a44f8dca0
RomThpt <romain@xrpl-commons.org>
2026-07-17 19:10:12 +0200

    Match ft_pikachu trivial functions

--- configure.py
+++ configure.py
@@ -1525,7 +1525,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_pikachu/fn_98_9D58.c"),
+        ],
     },
     {
         "lib": "ft_pikmin",
---
+++ src/mo_stub/ft_pikachu/fn_98_9D58.c
@@ -0,0 +1,14 @@
+void fn_98_9D58(void) {
+}
+
+void fn_98_9D5C(void) {
+}
+
+void fn_98_9D60(void) {
+}
+
+void fn_98_9D64(void) {
+}
+
+void fn_98_9D68(void) {
+}
