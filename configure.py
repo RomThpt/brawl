3e0351ef738e86e8b54a12b7147e7e725344a963
RomThpt <romain@xrpl-commons.org>
2026-07-18 01:22:40 +0200

    Match ft_captain trivial functions

--- configure.py
+++ configure.py
@@ -970,6 +970,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_captain/fn_100_C470.c"),
             Object(Matching, "mo_stub/ft_captain/fn_100_C558.c"),
             Object(Matching, "mo_stub/ft_captain/fn_100_D488.c"),
+            Object(Matching, "mo_stub/ft_captain/fn_100_D48C.c"),
         ],
     },
     {
