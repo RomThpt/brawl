7af85c910a6e705ef182638881b432a98d5e861c
RomThpt <romain@xrpl-commons.org>
2026-07-17 19:40:23 +0200

    Match sora_adv_stage trivial functions

--- configure.py
+++ configure.py
@@ -2086,6 +2086,7 @@ config.libs = [
             Object(Matching, "mo_stub/sora_adv_stage/fn_40_1DA50.c"),
             Object(Matching, "mo_stub/sora_adv_stage/fn_40_1DA58.c"),
             Object(Matching, "mo_stub/sora_adv_stage/fn_40_1DA94.c"),
+            Object(Matching, "mo_stub/sora_adv_stage/fn_40_1DA98.c"),
         ],
     },
     {
---
+++ src/mo_stub/sora_adv_stage/fn_40_1DA98.c
@@ -0,0 +1,15 @@
+void fn_40_1DA98(void) {
+}
+
+void fn_40_1DA9C(void) {
+}
+
+void fn_40_1DAA0(void) {
+}
+
+void fn_40_1DAA4(void) {
+}
+
+int fn_40_1DAA8(void) {
+    return 0;
+}
