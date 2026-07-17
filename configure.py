dae3928d03c3463737b426b8649bc37eb9cc5343
RomThpt <romain@xrpl-commons.org>
2026-07-17 15:46:25 +0200

    Match st_crayon trivial functions

--- configure.py
+++ configure.py
@@ -1832,6 +1832,7 @@ config.libs = [
             Object(Matching, "mo_stub/st_battles/fn_42_4BC.c"),
             Object(Matching, "mo_stub/st_crayon/fn_56_500C.c"),
             Object(Matching, "mo_stub/st_crayon/fn_56_512C.c"),
+            Object(Matching, "mo_stub/st_crayon/fn_56_536C.c"),
         ],
     },
     {
