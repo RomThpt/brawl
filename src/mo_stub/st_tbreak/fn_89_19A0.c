c9eedea80105eddc3ee3365689a86b460a3745dd
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:19:50 +0200

    Match st_tbreak trivial functions

---
+++ src/mo_stub/st_tbreak/fn_89_19A0.c
@@ -0,0 +1,18 @@
+int fn_89_19A0(void* p) {
+    return *(unsigned char*)((char*)p + 388);
+}
+
+int fn_89_19A8(void) {
+    return 0;
+}
+
+int fn_89_19B0(void) {
+    return 0;
+}
+
+int fn_89_19B8(void) {
+    return 0;
+}
+
+void fn_89_19C0(void) {
+}
