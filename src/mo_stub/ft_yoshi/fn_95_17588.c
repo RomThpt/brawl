6e3fe87aec9c19fcd9ee0e577d904fb36b23e212
RomThpt <romain@xrpl-commons.org>
2026-07-17 21:34:45 +0200

    Match ft_yoshi trivial functions

---
+++ src/mo_stub/ft_yoshi/fn_95_17588.c
@@ -0,0 +1,18 @@
+int fn_95_17588(void* p) {
+    return *(int*)((char*)p + 12);
+}
+
+void fn_95_17590(void) {
+}
+
+int fn_95_17594(void* p) {
+    return *(int*)((char*)p + 8);
+}
+
+void fn_95_1759C(void* p, int q) {
+    *(int*)((char*)p + 8) = q;
+}
+
+int fn_95_175A4(void* p) {
+    return *(int*)((char*)p + 8);
+}
