cce796de87f7088db3f7b3776871e2e4784a9e1f
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:20:03 +0200

    Match st_tbreak trivial functions

--- configure.py
+++ configure.py
@@ -3728,6 +3728,8 @@ config.libs = [
             Object(Matching, "mo_stub/st_tbreak/fn_89_1930.c"),
             Object(Matching, "mo_stub/st_tbreak/fn_89_195C.c"),
             Object(Matching, "mo_stub/st_tbreak/fn_89_19A0.c"),
+            Object(Matching, "mo_stub/st_tbreak/fn_89_19E0.c"),
+            Object(Matching, "mo_stub/st_tbreak/fn_89_1A0C.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_tbreak/fn_89_19E0.c
@@ -0,0 +1,15 @@
+int fn_89_19E0(void) {
+    return 20;
+}
+
+int fn_89_19E8(int a0) {
+    return a0 + 104;
+}
+
+int fn_89_19F0(void) {
+    return 0;
+}
+
+int fn_89_19F8(void) {
+    return 0;
+}
