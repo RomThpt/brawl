47c4d1d98efc7e6492a2ac211737373666c72ad9
RomThpt <romain@xrpl-commons.org>
2026-07-18 01:05:07 +0200

    Match sora_enemy trivial functions

--- configure.py
+++ configure.py
@@ -3415,6 +3415,7 @@ config.libs = [
             Object(Matching, "mo_stub/sora_enemy/fn_41_34E60.c"),
             Object(Matching, "mo_stub/sora_enemy/fn_41_34F30.c"),
             Object(Matching, "mo_stub/sora_enemy/fn_41_350A4.c"),
+            Object(Matching, "mo_stub/sora_enemy/fn_41_350CC.c"),
         ],
     },
     {
---
+++ src/mo_stub/sora_enemy/fn_41_350CC.c
@@ -0,0 +1,18 @@
+int fn_41_350CC(void) {
+    return 0;
+}
+
+int fn_41_350D4(void) {
+    return 0;
+}
+
+int fn_41_350DC(void) {
+    return 1;
+}
+
+int fn_41_350E4(void) {
+    return 0;
+}
+
+void fn_41_350EC(void) {
+}
