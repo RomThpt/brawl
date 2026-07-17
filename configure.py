0aefdc5d383f2713b2142b536acdfffc3cc5d97b
RomThpt <romain@xrpl-commons.org>
2026-07-17 19:35:43 +0200

    Match ft_zako trivial functions

--- configure.py
+++ configure.py
@@ -1879,6 +1879,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_zako/fn_126_7264.c"),
             Object(Matching, "mo_stub/ft_zako/fn_126_7280.c"),
             Object(Matching, "mo_stub/ft_zako/fn_126_7290.c"),
+            Object(Matching, "mo_stub/ft_zako/fn_126_72AC.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_zako/fn_126_72AC.c
@@ -0,0 +1,16 @@
+void fn_126_72AC(void) {
+}
+
+void fn_126_72B0(void) {
+}
+
+void fn_126_72B4(void) {
+}
+
+int fn_126_72B8(void) {
+    return 0;
+}
+
+int fn_126_72C0(void) {
+    return 0;
+}
