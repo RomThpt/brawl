abf44ae8ceb43926e35c37b74f8015dfa6c72e29
RomThpt <romain@xrpl-commons.org>
2026-07-17 18:42:59 +0200

    Match ft_ganon trivial functions

--- configure.py
+++ configure.py
@@ -1128,7 +1128,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/ft_ganon/fn_109_8F24.c"),
+        ],
     },
     {
         "lib": "ft_iceclimber",
---
+++ src/mo_stub/ft_ganon/fn_109_8F24.c
@@ -0,0 +1,14 @@
+void fn_109_8F24(void) {
+}
+
+void fn_109_8F28(void) {
+}
+
+void fn_109_8F2C(void) {
+}
+
+void fn_109_8F30(void) {
+}
+
+void fn_109_8F34(void) {
+}
