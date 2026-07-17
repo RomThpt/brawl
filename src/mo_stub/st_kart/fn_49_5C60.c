a85bbceeccff14477a6251a1cf192c41e23cd302
RomThpt <romain@xrpl-commons.org>
2026-07-17 17:01:25 +0200

    Match st_kart trivial functions

---
+++ src/mo_stub/st_kart/fn_49_5C60.c
@@ -0,0 +1,19 @@
+int fn_49_5C60(void) {
+    return 0;
+}
+
+int fn_49_5C68(void) {
+    return 0;
+}
+
+int fn_49_5C70(void) {
+    return 1;
+}
+
+int fn_49_5C78(void) {
+    return 0;
+}
+
+int fn_49_5C80(void) {
+    return -1;
+}
