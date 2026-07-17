b607e9d84225eea60b88cf22b5a8271d2fe237d1
RomThpt <romain@xrpl-commons.org>
2026-07-17 20:51:46 +0200

    Match ft_ness trivial functions

--- configure.py
+++ configure.py
@@ -1931,6 +1931,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_ness/fn_101_15F00.c"),
             Object(Matching, "mo_stub/ft_ness/fn_101_15F1C.c"),
             Object(Matching, "mo_stub/ft_ness/fn_101_15F3C.c"),
+            Object(Matching, "mo_stub/ft_ness/fn_101_15F48.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_ness/fn_101_15F48.c
@@ -0,0 +1,14 @@
+void fn_101_15F48(void) {
+}
+
+void fn_101_15F4C(void) {
+}
+
+void fn_101_15F50(void) {
+}
+
+void fn_101_15F54(void) {
+}
+
+void fn_101_15F58(void) {
+}
