7d1756c9de2e1b77c8e1095cc7b08803f4698e4f
RomThpt <romain@xrpl-commons.org>
2026-07-18 01:32:30 +0200

    Match ft_dedede trivial functions

--- configure.py
+++ configure.py
@@ -1061,6 +1061,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_dedede/fn_117_21D68.c"),
             Object(Matching, "mo_stub/ft_dedede/fn_117_21EF8.c"),
             Object(Matching, "mo_stub/ft_dedede/fn_117_21FE0.c"),
+            Object(Matching, "mo_stub/ft_dedede/fn_117_23D0C.c"),
         ],
     },
     {
