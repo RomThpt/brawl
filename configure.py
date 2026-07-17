bb289b48111cf6ae67cd6550953e16035c1886f4
RomThpt <romain@xrpl-commons.org>
2026-07-17 16:18:12 +0200

    Match st_dxgreens trivial functions

--- configure.py
+++ configure.py
@@ -2500,6 +2500,8 @@ config.libs = [
             Object(Matching, "mo_stub/st_dxgreens/fn_79_3AAC.c"),
             Object(Matching, "mo_stub/st_dxgreens/fn_79_3BB8.c"),
             Object(Matching, "mo_stub/st_dxgreens/fn_79_3DF8.c"),
+            Object(Matching, "mo_stub/st_dxgreens/fn_79_3E0C.c"),
+            Object(Matching, "mo_stub/st_dxgreens/fn_79_3E1C.c"),
         ],
     },
     {
