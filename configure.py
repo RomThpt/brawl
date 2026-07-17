44264dec81873678446cd4fa29c647ab9d8e31a8
RomThpt <romain@xrpl-commons.org>
2026-07-18 01:39:29 +0200

    Match ft_diddy trivial functions

--- configure.py
+++ configure.py
@@ -1155,6 +1155,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_diddy/fn_115_D544.c"),
             Object(Matching, "mo_stub/ft_diddy/fn_115_D62C.c"),
             Object(Matching, "mo_stub/ft_diddy/fn_115_D714.c"),
+            Object(Matching, "mo_stub/ft_diddy/fn_115_DC9C.c"),
         ],
     },
     {
