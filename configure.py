4ba180cbf6fbb2340cfa10370b3c221f057ffbf1
RomThpt <romain@xrpl-commons.org>
2026-07-17 20:16:45 +0200

    Match ft_ganon trivial functions

--- configure.py
+++ configure.py
@@ -1298,6 +1298,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_ganon/fn_109_11690.c"),
             Object(Matching, "mo_stub/ft_ganon/fn_109_116A4.c"),
             Object(Matching, "mo_stub/ft_ganon/fn_109_116C4.c"),
+            Object(Matching, "mo_stub/ft_ganon/fn_109_11778.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_ganon/fn_109_11778.c
@@ -0,0 +1,18 @@
+int fn_109_11778(void) {
+    return 1;
+}
+
+void fn_109_11780(void) {
+}
+
+int fn_109_11784(void) {
+    return 0;
+}
+
+int fn_109_1178C(void) {
+    return 0;
+}
+
+int fn_109_11794(void) {
+    return 0;
+}
