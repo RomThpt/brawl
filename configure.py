55ab9495f4adbf27d93bcb4de6d621936e345c02
RomThpt <romain@xrpl-commons.org>
2026-07-18 12:12:58 +0200

    Match ft_yoshi trivial functions

--- configure.py
+++ configure.py
@@ -3536,6 +3536,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_yoshi/fn_95_1B7EC.c"),
             Object(Matching, "mo_stub/ft_yoshi/fn_95_1D884.c"),
             Object(Matching, "mo_stub/ft_yoshi/fn_95_AF98.c"),
+            Object(Matching, "mo_stub/ft_yoshi/fn_95_B424.c"),
         ],
     },
     {
