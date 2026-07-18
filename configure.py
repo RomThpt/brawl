8fd9ba136c18bb5089910824b121fa96e31c6e89
RomThpt <romain@xrpl-commons.org>
2026-07-18 12:42:37 +0200

    Match sora_enemy trivial functions

--- configure.py
+++ configure.py
@@ -4102,6 +4102,7 @@ config.libs = [
             Object(Matching, "mo_stub/sora_enemy/fn_41_A870.c"),
             Object(Matching, "mo_stub/sora_enemy/fn_41_2B40C.c"),
             Object(Matching, "mo_stub/sora_enemy/fn_41_2B414.c"),
+            Object(Matching, "mo_stub/sora_enemy/fn_41_2B8A4.c"),
         ],
     },
     {
