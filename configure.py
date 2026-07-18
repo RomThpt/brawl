fae3267dacd5d055ac81564ddb9b1ade2e07a42e
RomThpt <romain@xrpl-commons.org>
2026-07-18 10:39:25 +0200

    Match ft_metaknight trivial functions

--- configure.py
+++ configure.py
@@ -2374,6 +2374,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_metaknight/fn_111_10FEC.c"),
             Object(Matching, "mo_stub/ft_metaknight/fn_111_1103C.c"),
             Object(Matching, "mo_stub/ft_metaknight/fn_111_5F38.c"),
+            Object(Matching, "mo_stub/ft_metaknight/fn_111_819C.c"),
         ],
     },
     {
