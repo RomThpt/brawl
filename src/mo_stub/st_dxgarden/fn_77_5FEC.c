b195cac3dea521577da9aeed79d0229365824128
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:17:50 +0200

    Match st_dxgarden trivial functions

---
+++ src/mo_stub/st_dxgarden/fn_77_5FEC.c
@@ -0,0 +1,25 @@
+extern int fn_77_3CA8();
+extern int fn_77_556C();
+extern int fn_77_34DC();
+extern int fn_77_3BDC();
+extern int fn_77_3BBC();
+
+int fn_77_5FEC(void* p) {
+    return fn_77_3CA8((char*)p + (-4));
+}
+
+int fn_77_5FF4(void* p) {
+    return fn_77_556C((char*)p + (-4));
+}
+
+int fn_77_5FFC(void* p) {
+    return fn_77_34DC((char*)p + (-4));
+}
+
+int fn_77_6004(void* p) {
+    return fn_77_3BDC((char*)p + (-4));
+}
+
+int fn_77_600C(void* p) {
+    return fn_77_3BBC((char*)p + (-4));
+}
