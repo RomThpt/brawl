eb2a96576b8a1c02d07f378c1c8e8a3ebe38fbc2
RomThpt <romain@xrpl-commons.org>
2026-07-18 12:05:49 +0200

    Match ft_wario trivial functions

--- configure.py
+++ configure.py
@@ -3418,6 +3418,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_wario/fn_110_13978.c"),
             Object(Matching, "mo_stub/ft_wario/fn_110_15470.c"),
             Object(Matching, "mo_stub/ft_wario/fn_110_1C8F0.c"),
+            Object(Matching, "mo_stub/ft_wario/fn_110_1FB68.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_wario/fn_110_1FB68.c
@@ -0,0 +1,3 @@
+int fn_110_1FB68(void) {
+    return 0;
+}
