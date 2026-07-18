ad433673b98949b0940332189a9bdc779e8f659a
RomThpt <romain@xrpl-commons.org>
2026-07-18 11:15:59 +0200

    Match ft_poke trivial functions

--- configure.py
+++ configure.py
@@ -2857,6 +2857,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_poke/fn_116_D50C.c"),
             Object(Matching, "mo_stub/ft_poke/fn_116_D5F4.c"),
             Object(Matching, "mo_stub/ft_poke/fn_116_D6DC.c"),
+            Object(Matching, "mo_stub/ft_poke/fn_116_D7C4.c"),
         ],
     },
     {
