d93db8ad533811e85a6d403bb7491c8ec454104b
RomThpt <romain@xrpl-commons.org>
2026-07-19 00:40:44 +0200

    Match sora_melee trivial functions

---
+++ src/mo_stub/sora_melee/fn_27_1074A4.c
@@ -0,0 +1,25 @@
+extern int fn_27_CA4FC();
+extern int fn_27_CA64C();
+extern int fn_27_CA6E0();
+extern int fn_27_CA6C0();
+extern int fn_27_CA710();
+
+int fn_27_1074A4(void* p) {
+    return fn_27_CA4FC((char*)p + (-4));
+}
+
+int fn_27_1074AC(void* p) {
+    return fn_27_CA64C((char*)p + (-4));
+}
+
+int fn_27_1074B4(void* p) {
+    return fn_27_CA6E0((char*)p + (-4));
+}
+
+int fn_27_1074BC(void* p) {
+    return fn_27_CA6C0((char*)p + (-4));
+}
+
+int fn_27_1074C4(void* p) {
+    return fn_27_CA710((char*)p + (-4));
+}
