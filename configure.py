48ddb7c8124f9e8ee587e94349f8fcba9beaa0f0
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:41:56 +0200

    Match st_mansion trivial functions

--- configure.py
+++ configure.py
@@ -6215,7 +6215,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/st_mansion/fn_47_9828.c"),
+        ],
     },
     {
         "lib": "st_mariopast",
---
+++ src/mo_stub/st_mansion/fn_47_9828.c
@@ -0,0 +1,25 @@
+extern int fn_47_4300();
+extern int fn_47_4FF0();
+extern int fn_47_4FD0();
+extern int fn_47_5020();
+extern int fn_47_5010();
+
+int fn_47_9828(void* p) {
+    return fn_47_4300((char*)p + (-4));
+}
+
+int fn_47_9830(void* p) {
+    return fn_47_4FF0((char*)p + (-4));
+}
+
+int fn_47_9838(void* p) {
+    return fn_47_4FD0((char*)p + (-4));
+}
+
+int fn_47_9840(void* p) {
+    return fn_47_5020((char*)p + (-4));
+}
+
+int fn_47_9848(void* p) {
+    return fn_47_5010((char*)p + (-4));
+}
