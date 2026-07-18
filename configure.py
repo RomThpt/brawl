f64fe543748767ad356d9937fbe81c9c950e4b9c
RomThpt <romain@xrpl-commons.org>
2026-07-18 11:36:46 +0200

    Match ft_samus trivial functions

--- configure.py
+++ configure.py
@@ -3076,6 +3076,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_samus/fn_94_E550.c"),
             Object(Matching, "mo_stub/ft_samus/fn_94_E638.c"),
             Object(Matching, "mo_stub/ft_samus/fn_94_E720.c"),
+            Object(Matching, "mo_stub/ft_samus/fn_94_E808.c"),
         ],
     },
     {
