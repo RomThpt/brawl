df94cf3eb4b74e17e8b8e80a9fe8df3b44401278
RomThpt <romain@xrpl-commons.org>
2026-07-18 18:27:20 +0200

    Match sora_melee trivial functions

---
+++ src/mo_stub/sora_melee/fn_27_7F4A4.c
@@ -0,0 +1,17 @@
+void fn_27_7F4A4(void) {
+}
+
+void fn_27_7F4A8(void) {
+}
+
+int fn_27_7F4AC(void) {
+    return 0;
+}
+
+int fn_27_7F4B4(void) {
+    return 0;
+}
+
+int fn_27_7F4BC(void) {
+    return -1;
+}
