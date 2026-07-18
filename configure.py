e46b62cf85dc5b1ff348ce91103cfb3dad613cff
RomThpt <romain@xrpl-commons.org>
2026-07-18 10:25:14 +0200

    Match ft_lucas trivial functions

--- configure.py
+++ configure.py
@@ -2079,6 +2079,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_lucas/fn_114_10348.c"),
             Object(Matching, "mo_stub/ft_lucas/fn_114_10430.c"),
             Object(Matching, "mo_stub/ft_lucas/fn_114_10518.c"),
+            Object(Matching, "mo_stub/ft_lucas/fn_114_1098C.c"),
         ],
     },
     {
