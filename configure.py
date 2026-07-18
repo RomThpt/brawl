289e3e3809b71efe3118ae7c21cab67a02694ddf
RomThpt <romain@xrpl-commons.org>
2026-07-18 10:58:56 +0200

    Match ft_pikmin trivial functions

--- configure.py
+++ configure.py
@@ -2691,6 +2691,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_pikmin/fn_113_F9DC.c"),
             Object(Matching, "mo_stub/ft_pikmin/fn_113_FAC4.c"),
             Object(Matching, "mo_stub/ft_pikmin/fn_113_FBAC.c"),
+            Object(Matching, "mo_stub/ft_pikmin/fn_113_FC94.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_pikmin/fn_113_FC94.c
@@ -0,0 +1,3 @@
+int fn_113_FC94(void) {
+    return 0;
+}
