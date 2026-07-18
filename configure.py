b67242afc75bb262776e70efa5f881fd6a2906b0
RomThpt <romain@xrpl-commons.org>
2026-07-18 11:26:11 +0200

    Match ft_robot trivial functions

--- configure.py
+++ configure.py
@@ -2994,6 +2994,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_robot/fn_120_9234.c"),
             Object(Matching, "mo_stub/ft_robot/fn_120_95E4.c"),
             Object(Matching, "mo_stub/ft_robot/fn_120_95E8.c"),
+            Object(Matching, "mo_stub/ft_robot/fn_120_95F0.c"),
         ],
     },
     {
