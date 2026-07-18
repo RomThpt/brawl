512fdcd38408f54c66916838b2214a06155eb67a
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:58:03 +0200

    Match st_plankton trivial functions

--- configure.py
+++ configure.py
@@ -7434,6 +7434,7 @@ config.libs = [
             Object(Matching, "mo_stub/st_plankton/fn_74_A588.c"),
             Object(Matching, "mo_stub/st_plankton/fn_74_A5B0.c"),
             Object(Matching, "mo_stub/st_plankton/fn_74_A5D8.c"),
+            Object(Matching, "mo_stub/st_plankton/fn_74_A600.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_plankton/fn_74_A600.c
@@ -0,0 +1,25 @@
+extern int fn_74_689C();
+extern int fn_74_687C();
+extern int fn_74_68CC();
+extern int fn_74_68BC();
+extern int fn_74_788C();
+
+int fn_74_A600(void* p) {
+    return fn_74_689C((char*)p + (-4));
+}
+
+int fn_74_A608(void* p) {
+    return fn_74_687C((char*)p + (-4));
+}
+
+int fn_74_A610(void* p) {
+    return fn_74_68CC((char*)p + (-4));
+}
+
+int fn_74_A618(void* p) {
+    return fn_74_68BC((char*)p + (-4));
+}
+
+int fn_74_A620(void* p) {
+    return fn_74_788C((char*)p + (-4));
+}
