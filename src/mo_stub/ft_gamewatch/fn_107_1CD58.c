095916338480197a7bc77ed3da9cceded55baf91
RomThpt <romain@xrpl-commons.org>
2026-07-18 14:06:21 +0200

    Match ft_gamewatch trivial functions

---
+++ src/mo_stub/ft_gamewatch/fn_107_1CD58.c
@@ -0,0 +1,22 @@
+extern int fn_107_1C750();
+extern int fn_107_5C00();
+
+int fn_107_1CD58(void* p) {
+    return fn_107_1C750((char*)p + (-64));
+}
+
+int fn_107_1CD60(void* p) {
+    return fn_107_1C750((char*)p + (-144));
+}
+
+int fn_107_1CD68(void* p) {
+    return fn_107_1C750((char*)p + (-136));
+}
+
+int fn_107_1CD70(void* p) {
+    return fn_107_5C00((char*)p + (-64));
+}
+
+int fn_107_1CD78(void* p) {
+    return fn_107_5C00((char*)p + (-144));
+}
