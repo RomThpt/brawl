5bfae8e056ddb8a375b07b562c449ba51d3bd173
RomThpt <romain@xrpl-commons.org>
2026-07-18 10:49:43 +0200

    Match ft_peach trivial functions

--- configure.py
+++ configure.py
@@ -2520,6 +2520,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_peach/fn_103_CA24.c"),
             Object(Matching, "mo_stub/ft_peach/fn_103_CAD4.c"),
             Object(Matching, "mo_stub/ft_peach/fn_103_CB84.c"),
+            Object(Matching, "mo_stub/ft_peach/fn_103_CC6C.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_peach/fn_103_CC6C.c
@@ -0,0 +1,3 @@
+int fn_103_CC6C(void) {
+    return 0;
+}
