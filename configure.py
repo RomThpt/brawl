eb17dc2d0abe26c471eb305cc232ebaa7e65a241
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:43:49 +0200

    Match st_mariopast trivial functions

--- configure.py
+++ configure.py
@@ -6232,7 +6232,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/st_mariopast/fn_48_7870.c"),
+        ],
     },
     {
         "lib": "st_metalgear",
---
+++ src/mo_stub/st_mariopast/fn_48_7870.c
@@ -0,0 +1,25 @@
+extern int fn_48_3AAC();
+extern int fn_48_3EBC();
+extern int fn_48_3E9C();
+extern int fn_48_3EEC();
+extern int fn_48_3EDC();
+
+int fn_48_7870(void* p) {
+    return fn_48_3AAC((char*)p + (-4));
+}
+
+int fn_48_7878(void* p) {
+    return fn_48_3EBC((char*)p + (-4));
+}
+
+int fn_48_7880(void* p) {
+    return fn_48_3E9C((char*)p + (-4));
+}
+
+int fn_48_7888(void* p) {
+    return fn_48_3EEC((char*)p + (-4));
+}
+
+int fn_48_7890(void* p) {
+    return fn_48_3EDC((char*)p + (-4));
+}
