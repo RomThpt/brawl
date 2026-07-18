845977eecdeb5d9abd2bd34bb45742e3676afa0f
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:32:49 +0200

    Match st_gw trivial functions

---
+++ src/mo_stub/st_gw/fn_63_FB38.c
@@ -0,0 +1,25 @@
+extern int fn_63_AE80();
+extern int fn_63_AE70();
+extern int fn_63_C94C();
+extern int fn_63_9E90();
+extern int fn_63_9A10();
+
+int fn_63_FB38(void* p) {
+    return fn_63_AE80((char*)p + (-4));
+}
+
+int fn_63_FB40(void* p) {
+    return fn_63_AE70((char*)p + (-4));
+}
+
+int fn_63_FB48(void* p) {
+    return fn_63_C94C((char*)p + (-4));
+}
+
+int fn_63_FB50(void* p) {
+    return fn_63_9E90((char*)p + (-4));
+}
+
+int fn_63_FB58(void* p) {
+    return fn_63_9A10((char*)p + (-4));
+}
