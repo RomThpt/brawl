2e39b4307ec6dbf54498bc2f0a2a22f3bd44bb23
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:32:23 +0200

    Match st_gw trivial functions

--- configure.py
+++ configure.py
@@ -6069,6 +6069,7 @@ config.libs = [
             Object(Matching, "mo_stub/st_gw/fn_63_FA70.c"),
             Object(Matching, "mo_stub/st_gw/fn_63_FA98.c"),
             Object(Matching, "mo_stub/st_gw/fn_63_FAC0.c"),
+            Object(Matching, "mo_stub/st_gw/fn_63_FAE8.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_gw/fn_63_FAE8.c
@@ -0,0 +1,25 @@
+extern int fn_63_9B4C();
+extern int fn_63_AEFC();
+extern int fn_63_AEEC();
+extern int fn_63_AF2C();
+extern int fn_63_AF1C();
+
+int fn_63_FAE8(void* p) {
+    return fn_63_9B4C((char*)p + (-4));
+}
+
+int fn_63_FAF0(void* p) {
+    return fn_63_AEFC((char*)p + (-4));
+}
+
+int fn_63_FAF8(void* p) {
+    return fn_63_AEEC((char*)p + (-4));
+}
+
+int fn_63_FB00(void* p) {
+    return fn_63_AF2C((char*)p + (-4));
+}
+
+int fn_63_FB08(void* p) {
+    return fn_63_AF1C((char*)p + (-4));
+}
