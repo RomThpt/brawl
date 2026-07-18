4584b8a1765134aa0e1c0c91196ab33de9e74a84
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:58:29 +0200

    Match st_stageedit trivial functions

--- configure.py
+++ configure.py
@@ -7450,7 +7450,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/st_stageedit/fn_85_4704.c"),
+        ],
     },
     {
         "lib": "st_starfox",
---
+++ src/mo_stub/st_stageedit/fn_85_4704.c
@@ -0,0 +1,10 @@
+extern void fn_85_4C54(void);
+extern void fn_85_4B84(void);
+
+void fn_85_4704(void) {
+    fn_85_4C54();
+}
+
+void fn_85_4708(void) {
+    fn_85_4B84();
+}
