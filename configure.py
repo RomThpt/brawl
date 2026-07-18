e3dbee929b7d57bee6fa688636231ae1f3ebcab5
RomThpt <romain@xrpl-commons.org>
2026-07-19 01:56:01 +0200

    Match sora_melee trivial functions

--- configure.py
+++ configure.py
@@ -6472,6 +6472,7 @@ config.libs = [
             Object(Matching, "mo_stub/sora_melee/fn_27_10788C.c"),
             Object(Matching, "mo_stub/sora_melee/fn_27_1078B4.c"),
             Object(Matching, "mo_stub/sora_melee/fn_27_1078DC.c"),
+            Object(Matching, "mo_stub/sora_melee/fn_27_107904.c"),
         ],
     },
     {
