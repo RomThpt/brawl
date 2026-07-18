f3e651530d6834f8e12887520a306a773dcd7277
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:34:47 +0200

    Match st_ice trivial functions

--- configure.py
+++ configure.py
@@ -6134,7 +6134,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/st_ice/fn_62_8408.c"),
+        ],
     },
     {
         "lib": "st_jungle",
