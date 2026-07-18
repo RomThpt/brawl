9a637819f21e9c9658a6f90709a34cf6b9863c49
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:42:21 +0200

    Match st_mansion trivial functions

--- configure.py
+++ configure.py
@@ -6218,6 +6218,7 @@ config.libs = [
         "objects": [
             Object(Matching, "mo_stub/st_mansion/fn_47_9828.c"),
             Object(Matching, "mo_stub/st_mansion/fn_47_9850.c"),
+            Object(Matching, "mo_stub/st_mansion/fn_47_9878.c"),
         ],
     },
     {
