55509e696b9fe0d65f4a27bed04fb83f49c2cd7b
RomThpt <romain@xrpl-commons.org>
2026-07-18 13:25:30 +0200

    Match st_dxgarden trivial functions

--- configure.py
+++ configure.py
@@ -4955,6 +4955,7 @@ config.libs = [
             Object(Matching, "mo_stub/st_dxgarden/fn_77_5564.c"),
             Object(Matching, "mo_stub/st_dxgarden/fn_77_5E80.c"),
             Object(Matching, "mo_stub/st_dxgarden/fn_77_6D0.c"),
+            Object(Matching, "mo_stub/st_dxgarden/fn_77_79C.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_dxgarden/fn_77_79C.c
@@ -0,0 +1,3 @@
+void fn_77_79C(void* p, int q) {
+    *(int*)((char*)p + 344) = q;
+}
