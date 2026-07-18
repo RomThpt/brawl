126aff9a4834ea23603f0915eedeb2c5ab68c786
RomThpt <romain@xrpl-commons.org>
2026-07-18 11:53:41 +0200

    Match ft_toonlink trivial functions

--- configure.py
+++ configure.py
@@ -3319,6 +3319,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_toonlink/fn_121_DAAC.c"),
             Object(Matching, "mo_stub/ft_toonlink/fn_121_E1B8.c"),
             Object(Matching, "mo_stub/ft_toonlink/fn_121_E1F4.c"),
+            Object(Matching, "mo_stub/ft_toonlink/fn_121_E2E8.c"),
         ],
     },
     {
