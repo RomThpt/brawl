498340abb759c34ae0e7375de48d0b9533962f92
RomThpt <romain@xrpl-commons.org>
2026-07-18 12:30:06 +0200

    Match ft_zelda trivial functions

--- configure.py
+++ configure.py
@@ -3657,6 +3657,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_zelda/fn_104_D8A0.c"),
             Object(Matching, "mo_stub/ft_zelda/fn_104_D988.c"),
             Object(Matching, "mo_stub/ft_zelda/fn_104_DA70.c"),
+            Object(Matching, "mo_stub/ft_zelda/fn_104_DB58.c"),
         ],
     },
     {
