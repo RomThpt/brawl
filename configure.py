231011fd48dc1557b30d9b4e4c7064dae400b130
RomThpt <romain@xrpl-commons.org>
2026-07-18 13:19:24 +0200

    Match sora_menu_tour trivial functions

--- configure.py
+++ configure.py
@@ -4491,7 +4491,9 @@ config.libs = [
         "mw_version": config.linker_version,
         "cflags": cflags_rel,
         "host": False,
-        "objects": [],
+        "objects": [
+            Object(Matching, "mo_stub/sora_menu_tour/fn_3_EDF4.c"),
+        ],
     },
     {
         "lib": "sora_menu_watch",
