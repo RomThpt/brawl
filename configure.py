28b5f0f894341def55e8ae45d4d3d4754c0893da
RomThpt <romain@xrpl-commons.org>
2026-07-19 01:06:25 +0200

    Match sora_melee trivial functions

--- configure.py
+++ configure.py
@@ -6461,6 +6461,7 @@ config.libs = [
             Object(Matching, "mo_stub/sora_melee/fn_27_1076D4.c"),
             Object(Matching, "mo_stub/sora_melee/fn_27_1076FC.c"),
             Object(Matching, "mo_stub/sora_melee/fn_27_107724.c"),
+            Object(Matching, "mo_stub/sora_melee/fn_27_10774C.c"),
         ],
     },
     {
---
+++ src/mo_stub/sora_melee/fn_27_10774C.c
@@ -0,0 +1,25 @@
+extern int fn_27_CC1B4();
+extern int fn_27_CC1A4();
+extern int fn_27_E8DE4();
+extern int fn_27_CC3D8();
+extern int fn_27_CC46C();
+
+int fn_27_10774C(void* p) {
+    return fn_27_CC1B4((char*)p + (-4));
+}
+
+int fn_27_107754(void* p) {
+    return fn_27_CC1A4((char*)p + (-4));
+}
+
+int fn_27_10775C(void* p) {
+    return fn_27_E8DE4((char*)p + (-4));
+}
+
+int fn_27_107764(void* p) {
+    return fn_27_CC3D8((char*)p + (-4));
+}
+
+int fn_27_10776C(void* p) {
+    return fn_27_CC46C((char*)p + (-4));
+}
