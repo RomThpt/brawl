bb1b61175c1e2c2ee2a651c9edd51521b9098e79
RomThpt <romain@xrpl-commons.org>
2026-07-18 17:20:03 +0200

    Match sora_melee trivial functions

---
+++ src/mo_stub/sora_melee/fn_27_7D948.c
@@ -0,0 +1,18 @@
+int fn_27_7D948(void) {
+    return 0;
+}
+
+int fn_27_7D950(void) {
+    return 0;
+}
+
+void fn_27_7D958(void) {
+}
+
+int fn_27_7D95C(void) {
+    return 0;
+}
+
+int fn_27_7D964(void) {
+    return 0;
+}
