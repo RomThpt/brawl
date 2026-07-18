5c3dc1c0a1a3bb197ea2eb99df88835f4917b8ce
RomThpt <romain@xrpl-commons.org>
2026-07-18 14:38:53 +0200

    Match ft_poke trivial functions

---
+++ src/mo_stub/ft_poke/fn_116_1A570.c
@@ -0,0 +1,25 @@
+extern int fn_116_183D8();
+extern int fn_116_1829C();
+extern int fn_116_1855C();
+extern int fn_116_184F8();
+extern int fn_116_185BC();
+
+int fn_116_1A570(void* p) {
+    return fn_116_183D8((char*)p + (-84));
+}
+
+int fn_116_1A578(void* p) {
+    return fn_116_1829C((char*)p + (-100));
+}
+
+int fn_116_1A580(void* p) {
+    return fn_116_1855C((char*)p + (-124));
+}
+
+int fn_116_1A588(void* p) {
+    return fn_116_184F8((char*)p + (-124));
+}
+
+int fn_116_1A590(void* p) {
+    return fn_116_185BC((char*)p + (-232));
+}
