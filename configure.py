7ee602eef826fed40bfe1aef5526c8b3b23a957d
RomThpt <romain@xrpl-commons.org>
2026-07-18 02:55:16 +0200

    Match ft_lucario trivial functions

--- configure.py
+++ configure.py
@@ -1983,6 +1983,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_lucario/fn_118_13DC4.c"),
             Object(Matching, "mo_stub/ft_lucario/fn_118_13DEC.c"),
             Object(Matching, "mo_stub/ft_lucario/fn_118_13E3C.c"),
+            Object(Matching, "mo_stub/ft_lucario/fn_118_6D04.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_lucario/fn_118_6D04.c
@@ -0,0 +1,3 @@
+int fn_118_6D04(void* p) {
+    return *(unsigned char*)((char*)p + 4);
+}
