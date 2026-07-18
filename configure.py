e5cd09761ce96e8062c9a12313e4eed0d8a064bc
RomThpt <romain@xrpl-commons.org>
2026-07-18 14:47:25 +0200

    Match ft_sonic trivial functions

--- configure.py
+++ configure.py
@@ -3846,6 +3846,8 @@ config.libs = [
             Object(Matching, "mo_stub/ft_sonic/fn_123_A004.c"),
             Object(Matching, "mo_stub/ft_sonic/fn_123_A00C.c"),
             Object(Matching, "mo_stub/ft_sonic/fn_123_A04C.c"),
+            Object(Matching, "mo_stub/ft_sonic/fn_123_EF30.c"),
+            Object(Matching, "mo_stub/ft_sonic/fn_123_EF80.c"),
         ],
     },
     {
