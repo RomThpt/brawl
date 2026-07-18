e8c1167ddc44941536d82645228291caa0c4cf91
RomThpt <romain@xrpl-commons.org>
2026-07-18 15:14:59 +0200

    Match st_crayon trivial functions

---
+++ src/mo_stub/st_crayon/fn_56_B164.c
@@ -0,0 +1,25 @@
+extern int fn_56_5A50();
+extern int fn_56_66E0();
+extern int fn_56_66C0();
+extern int fn_56_6710();
+extern int fn_56_6700();
+
+int fn_56_B164(void* p) {
+    return fn_56_5A50((char*)p + (-4));
+}
+
+int fn_56_B16C(void* p) {
+    return fn_56_66E0((char*)p + (-4));
+}
+
+int fn_56_B174(void* p) {
+    return fn_56_66C0((char*)p + (-4));
+}
+
+int fn_56_B17C(void* p) {
+    return fn_56_6710((char*)p + (-4));
+}
+
+int fn_56_B184(void* p) {
+    return fn_56_6700((char*)p + (-4));
+}
