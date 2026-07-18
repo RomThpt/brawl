d544c1fdab79831910cbd755c179e43ff1fb9aef
RomThpt <romain@xrpl-commons.org>
2026-07-18 10:36:10 +0200

    Match ft_mario trivial functions

--- configure.py
+++ configure.py
@@ -2211,6 +2211,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_mario/fn_91_D710.c"),
             Object(Matching, "mo_stub/ft_mario/fn_91_D7F8.c"),
             Object(Matching, "mo_stub/ft_mario/fn_91_D8E0.c"),
+            Object(Matching, "mo_stub/ft_mario/fn_91_D9C8.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_mario/fn_91_D9C8.c
@@ -0,0 +1,3 @@
+int fn_91_D9C8(void) {
+    return 0;
+}
