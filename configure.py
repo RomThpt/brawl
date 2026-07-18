4e7dbb8ce1e6dd6181c3c4122369c55b6f88aa07
RomThpt <romain@xrpl-commons.org>
2026-07-18 02:42:59 +0200

    Match ft_koopa trivial functions

--- configure.py
+++ configure.py
@@ -1840,6 +1840,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_koopa/fn_102_BD98.c"),
             Object(Matching, "mo_stub/ft_koopa/fn_102_BE80.c"),
             Object(Matching, "mo_stub/ft_koopa/fn_102_D554.c"),
+            Object(Matching, "mo_stub/ft_koopa/fn_102_D60C.c"),
         ],
     },
     {
