46f740b0fcea4b32288d10f6aacd635131cae86e
RomThpt <romain@xrpl-commons.org>
2026-07-18 12:16:40 +0200

    Match ft_yoshi trivial functions

--- configure.py
+++ configure.py
@@ -3545,6 +3545,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_yoshi/fn_95_E9C0.c"),
             Object(Matching, "mo_stub/ft_yoshi/fn_95_EAA8.c"),
             Object(Matching, "mo_stub/ft_yoshi/fn_95_EB90.c"),
+            Object(Matching, "mo_stub/ft_yoshi/fn_95_EC78.c"),
         ],
     },
     {
---
+++ src/mo_stub/ft_yoshi/fn_95_EC78.c
@@ -0,0 +1,3 @@
+int fn_95_EC78(void) {
+    return 0;
+}
