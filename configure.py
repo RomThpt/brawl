89a5293e2f29fa4569b85800f101ffb804e7010a
RomThpt <romain@xrpl-commons.org>
2026-07-18 13:31:13 +0200

    Match st_gw trivial functions

--- configure.py
+++ configure.py
@@ -5119,6 +5119,7 @@ config.libs = [
             Object(Matching, "mo_stub/st_gw/fn_63_3988.c"),
             Object(Matching, "mo_stub/st_gw/fn_63_933C.c"),
             Object(Matching, "mo_stub/st_gw/fn_63_9344.c"),
+            Object(Matching, "mo_stub/st_gw/fn_63_F88C.c"),
         ],
     },
     {
---
+++ src/mo_stub/st_gw/fn_63_F88C.c
@@ -0,0 +1,3 @@
+int fn_63_F88C(void) {
+    return 0;
+}
