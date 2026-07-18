15be067961d06979289bae100411013e828e024e
RomThpt <romain@xrpl-commons.org>
2026-07-18 02:01:25 +0200

    Match ft_fox trivial functions

--- configure.py
+++ configure.py
@@ -1356,6 +1356,7 @@ config.libs = [
             Object(Matching, "mo_stub/ft_fox/fn_97_E44C.c"),
             Object(Matching, "mo_stub/ft_fox/fn_97_E534.c"),
             Object(Matching, "mo_stub/ft_fox/fn_97_E61C.c"),
+            Object(Matching, "mo_stub/ft_fox/fn_97_10930.c"),
         ],
     },
     {
