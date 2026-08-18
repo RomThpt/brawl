38e50d3c67f187653e043cdb46e1054a2ca6ce49
RomThpt <reeflk8@gmail.com>
2026-08-18 15:48:44 +0200

    match: cmMenuFixedController::init
    Match init byte-perfect: cache the camera reference for a single load,
    set the transform flags via the u16 mask (halfword access), use m_rotate
    (0x40) instead of m_flag2, and zero m_rot through a Vec2f local so the
    stack frame matches.

--- src/sora/cm/cm_controller_menu_fixed.cpp
+++ src/sora/cm/cm_controller_menu_fixed.cpp
@@ -34,19 +34,22 @@ void cmMenuFixedController::storeDefault() {
     unk8 = true;
 }
 
-// NONMATCHING
 void cmMenuFixedController::init() {
-    m_cameraManager->m_cameras[0].unkCC = unkC;
-    m_cameraManager->m_cameras[0].unkFA.m_flag7 = true;
-    m_cameraManager->m_cameras[0].m_targetPos.m_x = unk10.m_x;
-    m_cameraManager->m_cameras[0].m_targetPos.m_y = unk10.m_y;
-    m_cameraManager->m_cameras[0].m_targetPos.m_z = unk10.m_z;
-    m_cameraManager->m_cameras[0].unkFA.m_flag1 = true;
-    m_cameraManager->m_cameras[0].unkD0 = unk1C;
-    m_cameraManager->m_cameras[0].m_rot.m_x = 0.0f;
-    m_cameraManager->m_cameras[0].m_rot.m_y = 0.0f;
-    m_cameraManager->m_cameras[0].m_rot.m_z = 0.0f;
-    m_cameraManager->m_cameras[0].unkFA.m_flag2 = true;
+    gfCamera& cam = m_cameraManager->m_cameras[0];
+    Vec2f rot;
+    cam.unkCC = unkC;
+    cam.unkFA.m_mask |= 0x80;
+    cam.m_targetPos.m_x = unk10.m_x;
+    cam.m_targetPos.m_y = unk10.m_y;
+    cam.m_targetPos.m_z = unk10.m_z;
+    cam.unkFA.m_mask |= 0x2;
+    cam.unkD0 = unk1C;
+    rot.m_x = 0.0f;
+    rot.m_y = 0.0f;
+    cam.m_rot.m_x = rot.m_x;
+    cam.m_rot.m_y = rot.m_y;
+    cam.m_rot.m_z = 0.0f;
+    cam.unkFA.m_mask |= 0x40;
 }
 
 // TODO: cmMenuFixedController::update
