3f09e4dc5317c2e3e86862b808e87f5e7733fc7e
RomThpt <reeflk8@gmail.com>
2026-08-24 12:02:24 +0200

    match: sndSystem loadCommon empty stub

--- configure.py
+++ configure.py
@@ -886,6 +886,7 @@ config.libs = [
             Object(Matching, "sora/ef/ef_screen_handle.cpp"),
             Object(Matching, "sora/ec/ec_trace_mgr.cpp"),
             Object(Matching, "sora/snd/snd_init_thread.cpp"),
+            Object(Matching, "sora/snd/snd_system.cpp"),
             Object(Matching, "sora/mv/mv_THPAudioDecode.cpp"),
             Object(Matching, "sora/mv/mv_THPRead.cpp"),
             Object(Matching, "sora/cm/cm_controller_default.cpp", extra_cflags=["-RTTI off"]),
